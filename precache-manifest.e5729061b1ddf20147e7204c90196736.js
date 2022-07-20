9aa78d785d5698aa05720d2a713d03eab7e2566a
SachinthaLakshan <49986724+SachinthaLakshan@users.noreply.github.com>
2022-07-20 13:04:43 +0530

    Updates

---
+++ precache-manifest.e5729061b1ddf20147e7204c90196736.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98eaaead910cfbe92556d5a5bfce2573",
+    "url": "/fuelpassgenerator/index.html"
+  },
+  {
+    "revision": "f0b10c26744d172905f7",
+    "url": "/fuelpassgenerator/static/css/main.80a07311.chunk.css"
+  },
+  {
+    "revision": "748b334750aac252d32e",
+    "url": "/fuelpassgenerator/static/js/2.8433028b.chunk.js"
+  },
+  {
+    "revision": "067af1dea2cebc18abbb92bc2540faf3",
+    "url": "/fuelpassgenerator/static/js/2.8433028b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0b10c26744d172905f7",
+    "url": "/fuelpassgenerator/static/js/main.4d0514d0.chunk.js"
+  },
+  {
+    "revision": "98c7f06f938cf7426c9b",
+    "url": "/fuelpassgenerator/static/js/runtime-main.aaa46cd4.js"
+  }
+]);
